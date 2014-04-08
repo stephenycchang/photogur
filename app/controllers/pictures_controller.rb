1173bf815f8858dc0edd294c22b8c7a9ce1f13c2
Stephen Chang <stephenycchang@gmail.com>
2014-04-08 14:08:01 -0400

    Moving @pictures in pictures_controller.rb into database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,63 +1,29 @@
 class PicturesController < ApplicationController
   def index
-    @pictures =[
-      {
-      :title => "The old church on the cost of White Sea",
-      :artist => "Sergey Ershov",
-      :url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-      },
-      {
-      :title => "Sea Power",
-      :artist => "Stephen Scullion",
-      :url => "http://moicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      },
-      {
-      :title => "Into the Poppies",
-      :artist => "John Wilhelm",
-      :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-      }
-      ]
+    @pictures = Picture.all
   end
 
   def show
-    @pictures =[
-      {
-      :title => "The old church on the cost of White Sea",
-      :artist => "Sergey Ershov",
-      :url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-      },
-      {
-      :title => "Sea Power",
-      :artist => "Stephen Scullion",
-      :url => "http://moicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      },
-      {
-      :title => "Into the Poppies",
-      :artist => "John Wilhelm",
-      :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-      }
-      ]
-      @picture = @pictures[params[:id].to_i]
-    end
-
-    def new
-
-    end
-
-    def create
-      render :text => "Saving a picture, URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
-
-    end
-
-    def edit
-
-    end
-
-    def update
-
-    end
-
-    def destroy
-
-    end
+    @picture = Picture.find(params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+    render :text => "Saving a picture, URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
