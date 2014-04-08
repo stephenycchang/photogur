b6c76bb7a03c8a9d3bf8fd67f2b0207b02bc520e
Stephen Chang <stephenycchang@gmail.com>
2014-04-08 13:47:11 -0400

    Refactoring Step #4 to use dynamic routes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,53 +1,62 @@
 class PicturesController < ApplicationController
   def index
-
-  end
-
-  def picture0
-    @picture = {
+    @pictures =[
+      {
       :title => "The old church on the cost of White Sea",
       :artist => "Sergey Ershov",
       :url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-    }
-  end
-
-  def picture1
-    @picture = {
+      },
+      {
       :title => "Sea Power",
       :artist => "Stephen Scullion",
       :url => "http://moicao.s3.amazonaws.com/bitmaker/wave.jpg"
-    }
-  end
-
-  def picture2
-    @picture = {
+      },
+      {
       :title => "Into the Poppies",
       :artist => "John Wilhelm",
       :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-    }
+      }
+      ]
   end
 
   def show
+    @pictures =[
+      {
+      :title => "The old church on the cost of White Sea",
+      :artist => "Sergey Ershov",
+      :url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+      },
+      {
+      :title => "Sea Power",
+      :artist => "Stephen Scullion",
+      :url => "http://moicao.s3.amazonaws.com/bitmaker/wave.jpg"
+      },
+      {
+      :title => "Into the Poppies",
+      :artist => "John Wilhelm",
+      :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+      }
+      ]
+      @picture = @pictures[params[:id].to_i]
+    end
 
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
   end
-
-  def new
-
-  end
-
-  def create
-
-  end
-
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  def destroy
-
-  end
-end
