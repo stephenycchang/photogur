d096faa84cf51d8e0f1813d5936bb5c3fd1f7997
Stephen Chang <stephenycchang@gmail.com>
2014-04-08 13:17:35 -0400

    Making pictures show at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,46 @@
 class PicturesController < ApplicationController
   def index
-    
+    @pictures = [
+      {
+        :title => "The old church on the cost of White Sea",
+        :artist => "Sergey Ershov",
+        :url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+      },
+      {
+        :title => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url => "http://moicao.s3.amazonaws.com/bitmaker/wave.jpg"
+      },
+      {
+        :title => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+      }
+    ]
+  end
+
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
   end
 end
